1634d8bd8154e9b2dd4acd848a380e0fe7ae40e3
sagarpandya94 <sagarpandya94@gmail.com>
2023-12-07 10:08:06 -0800

    Editing homepage page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -14,9 +14,10 @@ const HomePage = () => {
       <div><img src={picture} alt="display"/></div>
       <div className="font-size-14 text-white">
         <h1>Sagar Pandya</h1>
-        <p> <h2>Full Stack Developer | Quality Engineering | Software Engineer </h2></p>
-        <p> I have completed Master's in Computer Science from San Francisco State University. I am currently seeking full-time opportunities in Software Engineering and Quality Engineering area.</p>
-        <p> <MdEmail /> sagarpandy94@gmail.com </p>
+       <p> <h2>Software Development Engineer in Test | Quality Engineering | Software Development Engineer </h2></p>
+       <p> As a Software Development Engineer In Test (SDET) at Informatica, I ensure the quality and performance of cloud-based data integration products, using various tools and frameworks such as RestAssured, Selenium, Testrail, Jenkins, and Jira. I work with a cross-functional team of developers, operations, and product managers, following agile methodology and sprint planning.</p>
+       <p> I have a Master of Science in Computer Science from San Francisco State University, where I learned and applied skills in react.js, redux, javascript, node.js, express.js, HTML, and cucumber. I have completed multiple certifications in ISTQB Foundation Level and Selenium Essential Training. I am passionate about software engineering and quality engineering, and I am always eager to learn new technologies and best practices.</p>
+       <p> <MdEmail /> sagarpandy94@gmail.com </p>
         <p> <MdPhone /> (415)712-5855</p>
         <p> <a href="https://www.linkedin.com/in/sagarpandya94/" className="text-golden font-size-24"> <SiLinkedin /> </a>
         <a href="https://github.com/sagarpandya94" className="text-golden font-size-24"> <FaGithub /> </a></p>
